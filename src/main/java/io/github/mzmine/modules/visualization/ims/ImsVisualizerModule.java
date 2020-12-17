4b15f730fecfb93516c47d048788461968dcbb68
Annexhc <ansgar.korf@wwu.de>
2020-12-17 08:26:58 +0100

    Only allow IMS visualizer for IMS raw data files

--- src/main/java/io/github/mzmine/modules/visualization/ims/ImsVisualizerModule.java
+++ src/main/java/io/github/mzmine/modules/visualization/ims/ImsVisualizerModule.java
@@ -18,15 +18,17 @@
 
 package io.github.mzmine.modules.visualization.ims;
 
+import java.util.Collection;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import io.github.mzmine.datamodel.IMSRawDataFile;
 import io.github.mzmine.datamodel.MZmineProject;
+import io.github.mzmine.datamodel.RawDataFile;
 import io.github.mzmine.modules.MZmineModuleCategory;
 import io.github.mzmine.modules.MZmineRunnableModule;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.taskcontrol.Task;
 import io.github.mzmine.util.ExitCode;
-import java.util.Collection;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 /**
  * ims plot module
@@ -34,42 +36,46 @@ import javax.annotation.Nullable;
  * @author Nakul Bharti (knakul853@gmail.com)
  */
 public class ImsVisualizerModule implements MZmineRunnableModule {
-    private static final String MODULE_NAME = "IMS visualizer";
-    private static final String MODULE_DESCRIPTION = "IMS visualizer";
+  private static final String MODULE_NAME = "IMS visualizer";
+  private static final String MODULE_DESCRIPTION = "IMS visualizer";
 
-    @Nonnull
-    @Override
-    public String getDescription() {
-        return MODULE_DESCRIPTION;
+  @Nonnull
+  @Override
+  public String getDescription() {
+    return MODULE_DESCRIPTION;
+  }
+
+  @Nonnull
+  @Override
+  public String getName() {
+    return MODULE_NAME;
+  }
+
+  @Nonnull
+  @Override
+  public ExitCode runModule(@Nonnull MZmineProject project, @Nonnull ParameterSet parameters,
+      @Nonnull Collection<Task> tasks) {
+    RawDataFile[] files = parameters.getParameter(ImsVisualizerParameters.dataFiles).getValue()
+        .getMatchingRawDataFiles();
+    for (RawDataFile file : files) {
+      if (!(file instanceof IMSRawDataFile)) {
+        continue;
+      }
+      Task newTask = new ImsVisualizerTask(parameters);
+      tasks.add(newTask);
     }
+    return ExitCode.OK;
+  }
 
-    @Nonnull
-    @Override
-    public String getName() {
-        return MODULE_NAME;
-    }
+  @Nonnull
+  @Override
+  public MZmineModuleCategory getModuleCategory() {
+    return MZmineModuleCategory.VISUALIZATIONRAWDATA;
+  }
 
-    @Nonnull
-    @Override
-    public ExitCode runModule(
-            @Nonnull MZmineProject project,
-            @Nonnull ParameterSet parameters,
-            @Nonnull Collection<Task> tasks) {
-
-        Task newTask = new ImsVisualizerTask(parameters);
-        tasks.add(newTask);
-        return ExitCode.OK;
-    }
-
-    @Nonnull
-    @Override
-    public MZmineModuleCategory getModuleCategory() {
-        return MZmineModuleCategory.VISUALIZATIONRAWDATA;
-    }
-
-    @Nullable
-    @Override
-    public Class<? extends ParameterSet> getParameterSetClass() {
-        return ImsVisualizerParameters.class;
-    }
+  @Nullable
+  @Override
+  public Class<? extends ParameterSet> getParameterSetClass() {
+    return ImsVisualizerParameters.class;
+  }
 }
